f9d7e60bf9cd1704df48d378ce2093c6a69d8c81
Sarah26-10 <sarah.quehl@gmail.com>
2024-03-12 15:54:59 +0100

    initialCommit: ODrive on Torquotronic

--- src/ODriveCAN.h
+++ src/ODriveCAN.h
@@ -8,61 +8,64 @@
 
 #define REQUEST_PENDING 0xff
 
-#define CREATE_CAN_INTF_WRAPPER(TIntf) \
-    ODriveCanIntfWrapper wrap_can_intf(TIntf& intf) { \
-        return { \
-            &intf, \
-            [](void* intf, uint32_t id, uint8_t length, const uint8_t* data) { return sendMsg(*(TIntf*)intf, id, length, data); }, \
-            [](void* intf) { pumpEvents(*(TIntf*)intf); } \
-        }; \
+#define CREATE_CAN_INTF_WRAPPER(TIntf)                                                                                              \
+    inline ODriveCanIntfWrapper wrap_can_intf(TIntf &intf)                                                                          \
+    {                                                                                                                               \
+        return {                                                                                                                    \
+            &intf,                                                                                                                  \
+            [](void *intf, uint32_t id, uint8_t length, const uint8_t *data) { return sendMsg(*(TIntf *)intf, id, length, data); }, \
+            [](void *intf) { pumpEvents(*(TIntf *)intf); }};                                                                        \
     }
 
-
-struct ODriveCanIntfWrapper {
-    bool sendMsg(uint32_t id, uint8_t length, const uint8_t* data) {
+struct ODriveCanIntfWrapper
+{
+    bool sendMsg(uint32_t id, uint8_t length, const uint8_t *data)
+    {
         return (*send_msg_)(can_intf_, id, length, data);
     }
-    void pump_events() {
+    void pump_events()
+    {
         (*pump_events_)(can_intf_);
     }
 
-    void* can_intf_;
-    bool (*send_msg_)(void* intf, uint32_t id, uint8_t length, const uint8_t* data);
-    void (*pump_events_)(void* intf);
+    void *can_intf_;
+    bool (*send_msg_)(void *intf, uint32_t id, uint8_t length, const uint8_t *data);
+    void (*pump_events_)(void *intf);
 };
 
-class ODriveCAN {
+class ODriveCAN
+{
 public:
-    ODriveCAN(const ODriveCanIntfWrapper& can_intf, uint32_t node_id)
-        : can_intf_(can_intf), node_id_(node_id) {};
+    ODriveCAN(const ODriveCanIntfWrapper &can_intf, uint32_t node_id)
+        : can_intf_(can_intf), node_id_(node_id){};
 
     /**
      * @brief Clear all errors on the ODrive.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
     bool clearErrors();
-    
+
     /**
      * @brief Tells the ODrive to change its axis state.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
     bool setState(ODriveAxisState requested_state);
-    
+
     /**
      * @brief Sets the control mode and input mode of the ODrive.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
     bool setControllerMode(uint8_t control_mode, uint8_t input_mode);
-    
+
     /**
      * @brief Sends a position setpoint with optional velocity and torque feedforward.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
@@ -70,7 +73,7 @@ public:
 
     /**
      * @brief Sends a velocity setpoint with optional torque feedforward.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
@@ -78,7 +81,7 @@ public:
 
     /**
      * @brief Sends a torque setpoint to the ODrive.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
@@ -86,7 +89,7 @@ public:
 
     /**
      * @brief Initiates a trapezoidal trajectory move to a specified position.
-     * 
+     *
      * This function returns immediately and does not check if the ODrive
      * received the CAN message.
      */
@@ -95,39 +98,43 @@ public:
     /**
      * @brief Registers a callback for ODrive feedback processing.
      */
-    void onFeedback(void (*callback)(Get_Encoder_Estimates_msg_t& feedback, void* user_data), void* user_data = nullptr) {
-        feedback_callback_ = callback; 
+    void onFeedback(void (*callback)(Get_Encoder_Estimates_msg_t &feedback, void *user_data), void *user_data = nullptr)
+    {
+        feedback_callback_ = callback;
         feedback_user_data_ = user_data;
     }
 
     /**
      * @brief Registers a callback for ODrive axis state feedback.
      */
-    void onStatus(void (*callback)(Heartbeat_msg_t& feedback, void* user_data), void* user_data = nullptr) {
-        axis_state_callback_ = callback; 
+    void onStatus(void (*callback)(Heartbeat_msg_t &feedback, void *user_data), void *user_data = nullptr)
+    {
+        axis_state_callback_ = callback;
         axis_state_user_data_ = user_data;
     }
 
     /**
      * @brief Processes received CAN messages for the ODrive.
      */
-    void onReceive(uint32_t id, uint8_t length, const uint8_t* data);
+    void onReceive(uint32_t id, uint8_t length, const uint8_t *data);
 
     /**
      * @brief Sends a request message and awaits a response.
-     * 
+     *
      * Blocks until the response is received or the timeout is reached. Returns
      * false if the ODrive does not respond within the specified timeout.
      */
-    template<typename T>
-    bool request(T& msg, uint16_t timeout_ms = 10) {
+    template <typename T>
+    bool request(T &msg, uint16_t timeout_ms = 10)
+    {
         requested_msg_id_ = msg.cmd_id;
         can_intf_.sendMsg(
             (node_id_ << ODriveCAN::kNodeIdShift) | msg.cmd_id,
-            0, // no data
+            0,      // no data
             nullptr // RTR=1
         );
-        if (!awaitMsg(timeout_ms)) return false;
+        if (!awaitMsg(timeout_ms))
+            return false;
         msg.decode_buf(buffer_);
         return true;
     }
@@ -135,15 +142,15 @@ public:
     /**
      * @brief Sends a specified message over CAN.
      */
-    template<typename T>
-    bool send(T& msg) {
+    template <typename T>
+    bool send(T &msg)
+    {
         uint8_t data[8] = {};
         msg.encode_buf(data);
         return can_intf_.sendMsg(
             (node_id_ << ODriveCAN::kNodeIdShift) | msg.cmd_id,
             msg.msg_length,
-            data
-        );
+            data);
     }
 
 private:
@@ -159,9 +166,9 @@ private:
     static const uint8_t kNodeIdShift = 5;
     static const uint8_t kCmdIdBits = 0x1F;
 
-    void* axis_state_user_data_;
-    void* feedback_user_data_;
-    
-    void (*axis_state_callback_)(Heartbeat_msg_t& feedback, void* user_data) = nullptr;
-    void (*feedback_callback_)(Get_Encoder_Estimates_msg_t& feedback, void* user_data) = nullptr;
+    void *axis_state_user_data_;
+    void *feedback_user_data_;
+
+    void (*axis_state_callback_)(Heartbeat_msg_t &feedback, void *user_data) = nullptr;
+    void (*feedback_callback_)(Get_Encoder_Estimates_msg_t &feedback, void *user_data) = nullptr;
 };
--- src/ODriveHardwareCAN.hpp
+++ src/ODriveHardwareCAN.hpp
@@ -1,32 +1,33 @@
 #pragma once
 
 #include "ODriveCAN.h"
-#include <api/HardwareCAN.h>
+#include <Arduino_MachineControl.h>
 
+using namespace machinecontrol;
 // Must be defined by the application
-void onCanMessage(const CanMsg& msg);
+void onCanMessage(const mbed::CANMessage &msg);
 
-static bool sendMsg(HardwareCAN& can_intf, uint32_t id, uint8_t length, const uint8_t* data) {
+static bool sendMsg(COMMClass &can_intf, uint32_t id, uint8_t length, const uint8_t *data)
+{
     // Note: Arduino_CAN does not support the RTR bit. The ODrive interprets
     // zero-length packets the same as RTR=1, but it creates the possibility of
     // collisions.
-    CanMsg msg(
-        (id & 0x80000000) ? CanExtendedId(id) : CanStandardId(id),
-        length,
-        data
-    );
-    return can_intf.write(msg) >= 0;
+    mbed::CANMessage msg = mbed::CANMessage(id, data, length);
+    return can_intf.can.write(msg) >= 0;
 }
 
-static void onReceive(const CanMsg& msg, ODriveCAN& odrive) {
-    odrive.onReceive(msg.id, msg.data_length, msg.data);
+static void onReceive(const mbed::CANMessage &msg, ODriveCAN &odrive)
+{
+    odrive.onReceive(msg.id, msg.len, msg.data);
 }
 
-static void pumpEvents(HardwareCAN& intf, int max_events = 100) {
-    // max_events prevents an infinite loop if messages come at a high rate
-    while (intf.available() && max_events--) {
-        onCanMessage(intf.read());
+static void pumpEvents(COMMClass &intf, int max_events = 100)
+{
+    mbed::CANMessage msg;
+    if (comm_protocols.can.read(msg))
+    {
+        onCanMessage(msg);
     }
 }
 
-CREATE_CAN_INTF_WRAPPER(HardwareCAN)
+CREATE_CAN_INTF_WRAPPER(COMMClass)
